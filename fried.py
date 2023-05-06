abec340d9f2608de33f0d585a206c0656219f98b
Erik P <122167559+EriktheRDev@users.noreply.github.com>
2023-05-06 14:26:44 -0400

    Update fried.py

--- fried.py
+++ fried.py
@@ -14,7 +14,7 @@ sg.theme("DarkTeal12") #Sets theme
 
 sg.Popup("Fried\n(c) 2023 Erik's Gadgets") #IP addition
 
-room = sg.Window(title="Kitchen", layout=[[sg.Image(filename="room.png")], [sg.Button("Start Tutorial"), sg.Button("Continue")]])
+room = sg.Window(title="Fried", layout=[[sg.Image(filename="fried_title_screen.png")], [sg.Button("Start Tutorial"), sg.Button("Continue")]])
 
 while True:
     event, values = room.Read()
